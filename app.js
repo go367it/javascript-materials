48e46698501ab2823a512b241545a28b1f1d4145
go367it <satyamkumar3001@gmail.com>
2021-01-03 17:41:49 +0530

    callback with javascript

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-class Person {
-    constructor(firstname, lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
+// callbacks with javascript
+const posts = [
+    {title: 'PostOne', body:'this is post one'},
+    {title: 'Post two', body:'this is post two'}
+];
 
-    greeting () {
-        return `Hello there ${this.firstname} ${this.lastname}`;
-    }
+function createPost(post, callback){
+    setTimeout(function(){
+        posts.push(post);
+        callback()
+    }, 2000)
 }
 
-class Customer extends Person {
-    constructor(firstname, lastname, phone, membership) {
-        super(firstname, lastname);
-
-        this.phone = phone;
-        this.membership = membership;
-    }
+function getPosts() {
+    setTimeout(function(){
+        let output = '';
+        posts.forEach(function(post){
+            output += `<li>${posts.title}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000)
 }
 
-const john = new Customer('john', 'Doe', '555-555-5555', 'Standard');
-
-console.log(john.greeting())
+createPost({title: 'Post three', body:'This is post three'}, getPosts)
