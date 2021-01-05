d800a9f5f0272c2631db82a64006d196ee9c0789
go367it <satyamkumar3001@gmail.com>
2021-01-05 23:58:37 +0530

    Arrow functions in javascript

---
+++ arrowFunctions.js
@@ -0,0 +1,14 @@
+// to print name using arrow functions
+const printHello = () =>{
+    console.log('Hi')
+}
+
+printHello()
+
+
+// to print name using arrow functions
+const printName = (name) =>{
+    console.log(`Hello ${name}`)
+}
+
+printName('Satyam')
